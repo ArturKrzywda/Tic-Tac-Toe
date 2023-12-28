607d66dff14efa6adcacf0c53bfcf90918d56e77
Artur <152915536+ArturKrzywda@users.noreply.github.com>
2023-12-28 20:15:03 +0100

    Visuals

--- Game.py
+++ Game.py
@@ -9,16 +9,35 @@ class Game():
         self.table = [[None,None,None],
                      [None,None,None],
                      [None,None,None]]
-    def reset(self): 
+    def reset(self, error=None): 
         self.table = [[None,None,None],
                      [None,None,None],
                      [None,None,None]]
+        os.system("cls")
+        print("+----------------------+")
+        print("| Tic Tac Toe by Artur |")
+        print("+----------------------+\n") 
+
+        if error == 1: print("Wrong number of players!")
+        if error == 2: print("Wrong value!")
+        if error == None: print()
+        try:
+            self.NumberOfPlayer = int(input("Game for 1 or 2 Player? 1/2\n>"))
+            if self.NumberOfPlayer == 1 or self.NumberOfPlayer == 2: return
+            self.reset(error=1)
+        except Exception as e:
+            if e == KeyboardInterrupt:
+                os.system('cls')
+                print("Game closed by user")
+                exit()
+            self.reset(error=2)
+   
     def istie(self):
         for y  in range(3):
              for x in range(3):
                   if self.table[y][x] == None: 
-                      print("tie w funkcji")
                       return False
+        print("tie w funkcji")          
         return True
 
     def isWin(self):
@@ -55,17 +74,24 @@ class Game():
                  return
     def Playermove(self):
         while True:
-            qy = int(input("Give row cordinate\n>"))
-            qx = int(input("Give colum cordinate\n>"))
-            qy -= 1
-            qx -= 1
-            if qy > 2 or qx > 2:
-                self.draw(error=1)
-            else:
-                if self.table[qy][qx] != None:
-                    self.draw(error=2)
-                else: break
-        
+            try:
+                qy = int(input("Give row cordinate\n>"))
+                qy -= 1
+                if qy > 2: self.draw(error=1)
+                else:
+                    qx = int(input("Give colum cordinate\n>"))
+                    qx -= 1
+                    if qx > 2:
+                        self.draw(error=1)
+                    else:
+                        if self.table[qy][qx] != None:
+                            self.draw(error=2)
+                        else: break
+            except Exception as e:
+                if e == KeyboardInterrupt:
+                    print("Game closed by user")
+                    exit()
+                self.draw(error=3)
         return [qy,qx]
          
          
@@ -81,30 +107,31 @@ class Game():
                     else:
                         drawtable[y].append('O')
             draw = [
-               ["    ",drawtable[0][0]," │ ",drawtable[0][1]," │ ",drawtable[0][2]," "],
-               ["   ───┼───┼───"],
-               ["    ",drawtable[1][0]," │ ",drawtable[1][1]," │ ",drawtable[1][2]," "],
-               ["   ───┼───┼───"],
-               ["    ",drawtable[2][0]," │ ",drawtable[2][1]," │ ",drawtable[2][2]," "],
-               ["\n"]
+                ["     1   2   3\n"],
+                ["1    ",drawtable[0][0]," │ ",drawtable[0][1]," │ ",drawtable[0][2]," "],
+                ["    ───┼───┼───"],
+                ["2    ",drawtable[1][0]," │ ",drawtable[1][1]," │ ",drawtable[1][2]," "],
+                ["    ───┼───┼───"],
+                ["3    ",drawtable[2][0]," │ ",drawtable[2][1]," │ ",drawtable[2][2]," "],
+                ["\n"]
             ]
             print("+----------------------+")
             print("| Tic Tac Toe by Artur |")
-            print("+----------------------+")
-            print('\n\n')
+            print("+----------------------+\n")
+            
             for t in draw:
                 row = ''.join(t)
                 print(row)
-           
-           
             
 
             if error == 1:
-                print("\n\nCordinates are out of range!")
+                print("Cordinates are out of range!")
             if error == 2:
-                print("\n\nThis place is taken!")
+                print("This place is taken!")
+            if error == 3:
+                print("Wrong value!")
             if error == None:
-                print("\n\n")
+                print()
             if self.NumberOfPlayer == 2:
                 if self.CurrentPlayerTurn == 1:
                     print("Player X move")
@@ -112,23 +139,10 @@ class Game():
                     print("Player O move")
             else: 
                 if self.CurrentPlayerTurn == 1: 
-                    print("\n\nPlayer X move")
+                    print("Player X move")
 
     def run(self, b):
         try:
-            print("Tic Tac Toe by arturek9")
-            while True:
-                try:
-                    self.NumberOfPlayer = int(input("Game for 1 or 2 Player? 1/2\n>"))
-                    if self.NumberOfPlayer == 1 or self.NumberOfPlayer == 2: break
-                    os.system('cls')
-                    print("Wrong number of players!")
-                except:
-                    os.system('cls')
-                    print("Wrong value!")
-                
-                
-
             self.reset()
             while True:
                 while True:
